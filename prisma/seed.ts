b20b52bd764ab3bf50483c99a153a9f033e17b64
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-12-22 04:25:43 +0100

    refactor: use `fast-csv` parser in the `get transactions`

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
 const fileSystem = require('fs')
-const CSVParser = require('csv-parser')
+//const CSVParser = require('csv-parser')
+import { parseStream as CSVParserStream } from 'fast-csv'
 
 import { PrismaClient, Account, Category, Transaction } from '@prisma/client'
 const prisma = new PrismaClient()
@@ -19,12 +20,18 @@ function getAccounts(): Promise<Account[]> {
   let fileOptions: FileOptions = { skipLines: 1, headers: ['id', 'name', 'bank'] }
 
   return new Promise((resolve, reject) => {
-    fileSystem
+    const accountsFile = fileSystem.createReadStream(getFilePath(filename))
+    CSVParserStream(accountsFile)
+      .on('error', (error: Error) => reject(error))
+      .on('data', (account: Account) => { accounts.push(account) })
+      .on('end', (accountCount: number) =>
+        console.log(`Parsed ${accountCount} rows`))
+    /* fileSystem
       .createReadStream(getFilePath(filename))
       .pipe(CSVParser(fileOptions))
-      .on('data', (account: Account) => accounts.push(account))
-      .on('end', () => { resolve(accounts) })
       .on('error', (error: Error) => reject(error))
+      .on('data', (account: Account) => accounts.push(account))
+      .on('end', () => { resolve(accounts) }) */
   })
 }
 
@@ -36,39 +43,54 @@ function getCategories(): Promise<Category[]> {
   return new Promise((resolve, reject) => {
     fileSystem
       .createReadStream(getFilePath(filename))
-      .pipe(CSVParser(fileOptions))
+      //.pipe(CSVParser(fileOptions))
+      .on('error', (error: Error) => reject(error))
       .on('data', (category: Category) => categories.push(category))
       .on('end', () => { resolve(categories) })
-      .on('error', (error: Error) => reject(error))
   })
-}
 
-function getTransactions(): Promise<Transaction[]> {
-  let transactions: Transaction[] = []
-  let filename: string = 'categories.csv'
-  let fileOptions: FileOptions = {
-    skipLines: 1,
-    headers: ['id', 'accountId', 'categoryId', 'reference', 'amount', 'currency', 'date']
+  function getTransactions(): Promise<Transaction[]> {
+    let transactions: Transaction[] = []
+    let filename: string = 'transactions.csv'
+
+    return new Promise((resolve, reject) => {
+      const transactionsFile = fileSystem.createReadStream(getFilePath(filename))
+      CSVParserStream(transactionsFile, { headers: true })
+        .on('error', (error: Error) => reject(error))
+        .on('data', (transaction: Transaction) => {
+          let amount = parseFloat(transaction.amount.toString())
+          let date = new Date(transaction.date)
+          let categoryId = transaction.categoryId ? transaction.categoryId : null
+          transactions.push({ ...transaction, categoryId, date, amount })
+        })
+        .on('end', () => {
+          transactions = transactions.slice(1, 501) // TODO: only for dev env
+          resolve(transactions)
+        })
+    })
   }
 
-  return new Promise((resolve, reject) => {
-    fileSystem
-      .createReadStream(getFilePath(filename))
-      .pipe(CSVParser(fileOptions))
-      .on('data', (transaction: Transaction) => transactions.push(transaction))
-      .on('end', () => { resolve(transactions) })
-      .on('error', (error: Error) => reject(error))
-  })
-}
+  async function main() {
+    /* const accounts = await getAccounts() */
+    /* const categories = await getCategories() */
+    const transactions = await getTransactions()
+    console.log(transactions)
 
-async function main() {
-}
+    /* await prisma.account.deleteMany()
+    await prisma.account.createMany({ data: accounts }) */
 
-main()
-  .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+    /* await prisma.category.deleteMany()
+    await prisma.category.createMany({ data: categories }) */
+
+    //await prisma.transaction.deleteMany()
+    //await prisma.transaction.createMany({ data: transactions })
+  }
+
+  main()
+    .catch((error) => {
+      console.error(error)
+      process.exit(1)
+    })
+    .finally(async () => {
+      await prisma.$disconnect()
+    })
