bf86d3880f7e499ae68649efd559a9e05f75ad9f
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-12-22 13:45:48 +0100

    chore: write context for the server

---
+++ src/context.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma: PrismaClient = new PrismaClient()
+
+export interface Context {
+  prisma: PrismaClient
+}
+
+export const context: Context = {
+  prisma
+}
